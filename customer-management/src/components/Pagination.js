f8c0afe97a2887c33218a4dc6396a511fb0cfe61
sreenathgandra12 <152074530+sreenathgandra12@users.noreply.github.com>
2024-03-04 10:17:48 +0530

    Update Pagination.js

--- customer-management/src/components/Pagination.js
+++ customer-management/src/components/Pagination.js
@@ -1,37 +1,18 @@
-/*// Importing React library
+// Importing React library
 import React from 'react';
-// Functional component for pagination
-const Pagination = ({ currentPage, totalPages, setCurrentPage }) => {
-  // Array to store page numbers
-  const pageNumbers = [];
- // Loop to generate page numbers
-  for (let i = 1; i <= totalPages; i++) {
-    pageNumbers.push(i);
-  }
- // JSX to render pagination
-  return (
-    <nav>
-      <ul className="pagination">
-        {pageNumbers.map(number => (
-          <li key={number} className="page-item">
-            <button
-              className={`page-link ${currentPage === number ? 'active' : ''}`}
-              onClick={() => setCurrentPage(number)}
-            >
-              {number}
-            </button>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
-};
 
-// Exporting the Pagination component
-export default Pagination;*/
-
-
-import React from 'react';
+/**
+* A functional component that renders a pagination for a list of customers.
+* It receives the following props:
+* 1. `customersPerPage`: The number of customers displayed per page.
+* 2. `totalPages`: The total number of pages required for the entire list of customers.
+* 3. `paginate`: A callback function that is triggered when a page number is clicked,
+*                updating the current page in the parent component.
+*
+* The component creates a list of page numbers based on the `totalPages` value,
+* rendering them as clickable links. When a page number is clicked, the `paginate`
+* function is invoked with the clicked page number as an argument.
+*/
 
 const Pagination = ({ customersPerPage, totalPages,paginate }) => {
   const pageNumbers = [];
@@ -43,8 +24,10 @@ const Pagination = ({ customersPerPage, totalPages,paginate }) => {
   return (
     <nav>
       <ul className="pagination">
+     {/* Map through the pageNumbers array and render each page number as a list item */}
         {pageNumbers.map(number => (
           <li key={number} className="page-item">
+           {/* Render each page number as a clickable link that triggers the paginate callback */}
             <a onClick={() => paginate(number)} href="!#" className="page-link">
               {number}
             </a>
